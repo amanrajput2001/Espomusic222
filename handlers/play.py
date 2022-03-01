56f20c245795f54402c3fdc10d3975ba02e96b23
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2022-03-01 10:57:39 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -98,7 +98,7 @@ async def play(_, message: Message):
     global que
     global useer
 
-    lel = await message.reply("🔎 **《 𝐓𝐒𝐆 𝐌𝐔𝐒𝐈𝐂 》..𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 ❤️ 𝐌𝐚𝐝𝐞 𝐁𝐲 〖 𝐀𝐌𝐀𝐍 𝐑𝐀𝐉𝐏𝐔𝐓 👿 〗......**")
+    lel = await message.reply("🔎 **《 𝐓𝐒𝐆 𝐌𝐔𝐒𝐈𝐂 》..𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 ❤️ 𝐌𝐚𝐝𝐞 𝐁𝐲 〖 [𝐀𝐌𝐀𝐍 𝐑𝐀𝐉𝐏𝐔𝐓 👿](https://t.me/itzamanrajput) 〗......**")
 
     administrators = await get_administrators(message.chat)
     chid = message.chat.id
